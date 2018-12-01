b1b02f8390ff6821a9662dccad686d7e2fa9c63a
Publicemail <toropumihai@gmail.com>
2018-12-01 23:24:25 +0200

    bbeaaaufort

---
+++ beaufort.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+#include <stdlib.h>
+
+
+#define NR 64
+
+char BeaufortEncrypt(char *alphabet, int *intKey, char text, int position)
+{
+    int encryptedText;
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (text == alphabet[i])
+        {
+            encryptedText = (i + intKey[position]) % 26;
+            return alphabet[encryptedText];
+        }
+    }
+}
+
+char BeaufortDecrypt(char *alphabet, int *intKey, char text, int position)
+{
+    int decryptedText;
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (text == alphabet[i])
+        {
+            decryptedText = (i - intKey[position]) % 26;
+            return alphabet[decryptedText];
+        }
+    }
+}
+
+int main() {
+    char plainText[NR], alphabet[27] = "abcdefghijklmnopqrstuvwxyz", key[NR], encryptedText[NR], decryptedText[NR];
+    int intKey[NR], position = 0;
+    printf("plaintext:\n");
+    gets(plainText);
+    printf("key\n");
+    gets(key);
+
+    int textLength = strlen(plainText);
+    int keyLength = strlen(key);
+
+    for (int i = 0; i < keyLength; i++) {
+        for(int j = 0; j < 26; j++)
+        {
+            if (key[j] == alphabet[i])
+            {
+                intKey[j] = i;
+            }
+        }
+    }
+
+    int x = 1;
+    printf("\nChoose:\n1.Encrypt text.\n2.Decrypt text.\n");
+    scanf("%d", &x);
+
+    switch (x)
+    {
+        case 1:
+            for(int i = 0; i < textLength; i++)
+            {
+                encryptedText[i] = BeaufortEncrypt(alphabet, intKey, plainText[i], position % keyLength);
+                ++position;
+            }
+            encryptedText[textLength] = '\0';
+            printf("\nEncrypted text: %s\n", encryptedText);
+            break;
+
+        case 2:
+            for(int i = 0; i < textLength; i++)
+            {
+                decryptedText[i] = BeaufortDecrypt(alphabet, intKey, plainText[i], position % keyLength);
+                ++position;
+            }
+            decryptedText[textLength] = '\0';
+            printf("\nDecrypted text: %s\n", decryptedText);
+            break;
+
+        default:
+            printf("Exit");
+    }
+
+    return 0;
+}
